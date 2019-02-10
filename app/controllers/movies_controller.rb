d7f783b5389930a71a67b61b1d6dad24dcba5c2f
EC2 Default User <ec2-user@ip-172-31-15-54.us-east-2.compute.internal>
2019-02-10 23:50:13 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,14 +12,57 @@ class MoviesController < ApplicationController
 
   def index
     # @movies = Movie.all
-    @movies = Movie.order(params[:sort_by])
-    @sort_type = params[:sort_by]
+    # @movies = Movie.order(params[:sort_by])
+    # @sort_type = params[:sort_by]
     
     @all_ratings = Movie.all_ratings
-    if params[:ratings]
-      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    # if params[:ratings]
+    #   @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    # end
+    # @sel_ratings = params[:ratings]
+    # if !@sel_ratings
+    #   @sel_ratings = Hash.new
+    # end
+    
+    redirect = false
+    if params[:sort_by]
+      @movies = Movie.order(params[:sort_by])
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+      elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
     end
-    @sel_ratings = params[:ratings]
+    
+    if params[:commit] == "Refresh" and params[:ratings] == nil
+      @sel_ratings = nil
+      session[:ratings] = nil
+      elsif params[:ratings] 
+      @sel_ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+      elsif session[:ratings]
+      @sel_ratings = session[:ratings]
+      redirect = true
+    else
+      @sel_ratings = nil
+    end
+    
+    if redirect
+      flash.keep
+      redirect_to movies_path(:sort_by => @sort_by, :ratings => @sel_ratings)
+    end
+    
+    if @sort_by and @sel_ratings
+      @movies = Movie.where(:rating => @sel_ratings.keys).order(@sort_by)
+      elsif @sel_ratings
+      @movies = Movie.where(:rating => @sel_ratings.keys)
+      elsif @sort_by
+      @movies = Movie.order(@sort_by)
+    else
+      @movies = Movie.all
+    end
     if !@sel_ratings
       @sel_ratings = Hash.new
     end
